6a91712e310533ceb0200e2f9050de45f788fbae
Sunil Shrestha <ilamalisunilshrestha00@gmail.com>
2018-09-18 08:00:49 +0545

    new learn js and time js file is added

--- js/array.js
+++ js/array.js
@@ -1,13 +1,17 @@
 
 
-function getcomputedata(){
-return data.map(function(item, index){
-console.log(+item*5);
-return +item*5;
+function getcomputedata() {
+        return data.map(function (item, index) {
+                console.log(+item * 5);
+                return +item * 5;
 
 
-})
-.filter(function(item, index) {  
-        return item % 2 === 0;    })    
-.reduce(function(total, item) {   
-       return total + item;    });}console.log('Result is', getComputatedValue(myNumbers));
+        })
+                .filter(function (item, index) {
+                        return item % 2 === 0;
+                })
+                .reduce(function (total, item) {
+                        return total + item;
+                });
+}
+console.log('Result is', getComputatedValue(myNumbers));
--- js/form.js
+++ js/form.js
@@ -1,31 +1,61 @@
-function feedBackform(){
-var firstname=document.getElementById("firstname").value
+function feedBackform() {
+    var firstname = document.getElementById("firstname").value
 
 
 }
 
-function validateName(){
-let  firstname=document.getElementById("firstname").value
-if(firstname===""){
- let error=document.getElementById('error-first-name');
- error.innerHTML = "First Name cannot be empty" ;
- error.style.color="red";
-}else{
+function validateName() {
+    let firstname = document.getElementById("firstname").value
+    if (firstname === "") {
+        let error = document.getElementById('error-first-name');
+        error.innerHTML = "First Name cannot be empty";
+        error.style.color = "red";
+    } else {
 
-let error=document.getElementById("error-first-name");
-    error.innerHTML="First Name is added sucessfully ";
-error.style.color="green";
-}
-
-}
-filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-function validateEmail(){
-
-let  email=document.getElementById("email").value;
-if(email==="" &&!email.filter ){
-let erroremail=document.getElementById("error-email-id");
-erroremail.innerHTML=""
+        let error = document.getElementById("error-first-name");
+        error.innerHTML = "";
+        error.style.color = "green";
+    }
 
 }
+
+function validateEmail() {
+    let emailid = ['sunil@gmail.com', 'su@gmail.com']
+    let email =document.getElementById("email").value;
+   
+    if (email === emailid) {
+        let erroremail = document.getElementById("error-email-id");
+        erroremail.innerHTML = "email is sucessfully match";
+        erroremail.style.color = "green";
+    } else {
+        erroremail.innerHTML = "enter valid enail";
+        erroremail.style.color = "red";
+
+    }
+    function genderVerification() {
+        let gendervalue = document.getElementsByClassName("gender").value;
+        if (gendervalue === "male") {
+let 
+
+        } else if (gendervalue === "female") {
+
+
+
+            
+
+        } else {
+
+
+        }
+
+
+
+    }
+}
+setTimeout(function()){
+    var FormDom=document.getElementById("");
+    FormDom.style.display="null";},500}
+
+
 
 }
